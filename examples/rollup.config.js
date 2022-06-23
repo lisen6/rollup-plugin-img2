cfa7eb061b5ca871795f28b4f44446eb358342b1
sen.li <1789502923@qq.com>
2022-06-23 20:05:08 +0800

    add test case

--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -1,5 +1,5 @@
 import html from '@rollup/plugin-html'
-import image from '../src/index'
+import image from 'rollup-plugin-img2'
 
 export default {
   input: 'src/index.js',
@@ -11,7 +11,7 @@ export default {
     image({
       output: 'dist/images',
       hash: true,
-      limit: 2048,
+      limit: 1024 * 10,
       _slash: true
     }),
     html()
